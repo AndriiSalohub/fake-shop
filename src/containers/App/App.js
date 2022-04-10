907fd8c8224c37498d6fb635ae694b288c80f443
Andrii <aominedaikivs@gmail.com>
2022-04-10 15:12:04 +0300

    test commit copy repository

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,7 +3,6 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { Header } from 'containers/Header/Header'
 import { Main } from 'containers/Main/Main'
 import { Footer } from 'containers/Footer/Footer'
-import { productsArray } from 'components/products/productsArray'
 
 export const App = () => {
     const [productsInCart, setproductsInCart] = useState({
